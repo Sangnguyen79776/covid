161a3516e6ec3d1e920bebe89554e8eef7ffbe82
Sangnguyen79776 <145439978+Sangnguyen79776@users.noreply.github.com>
2023-12-17 10:49:44 -0600

    Update config.php

--- covid-tms/includes/config.php
+++ covid-tms/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 //time zone
-date_default_timezone_set('Asia/VietNam');
+date_default_timezone_set('Asia/Kolkata');
 //database connection
 $con=mysqli_connect("localhost","root","","covidtmsdb");
 if(mysqli_connect_errno()){
